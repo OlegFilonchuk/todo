d2987c28bfef59c0bbe255682e36a081a7907748
Oleg Filonchuk <oifilonchuk@gmail.com>
2018-12-13 14:54:32 +0200

    add some components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,24 @@
-import React, {Component} from 'react'
+import React from 'react'
 import TodoListItem from './TodoListItem'
 
-class TodoList extends Component {
-  render() {
-    const items = ['Learn React', 'Build Awesome App']
+const TodoList = ({ todos }) => {
+
+  const list = todos.map((item) => {
+
+    const { id, ...itemProps} = item
+
     return (
-      <ul>
-        <li><TodoListItem label={items[0]}/></li>
-        <li>
-          <TodoListItem
-            label={items[1]}
-            important
-          />
-        </li>
-      </ul>
+      <li key={id}>
+        <TodoListItem {...itemProps}/>
+      </li>
     )
-  }
+  })
+
+  return (
+    <ul>
+      {list}
+    </ul>
+  )
 }
 
 export default TodoList
