4d48ce39e398aecfa45cf812025763ceff6148dc
Oleg Filonchuk <oifilonchuk@gmail.com>
2018-12-20 10:37:11 +0200

    add filters css

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -7,25 +7,47 @@ export default class ItemStatusFilter extends Component {
     filter: 'all'
   }
 
-  onButtonClick = (ev) => {
+  onButtonClick = ({target}) => {
     this.setState({
-      filter: ev.target.dataset.filter
+      filter: target.dataset.filter
     })
-    this.props.onFilterChange(ev.target.dataset.filter)
+    this.props.onFilterChange(target.dataset.filter)
+
+    document.querySelectorAll('[data-active]').forEach((item) => {
+      item.dataset.active = 'false'
+    })
+
+    target.dataset.active = 'true'
   }
 
   render() {
     return (
       <div className='btn-group'>
-        <button className="btn btn-outline-secondary" data-filter="all" onClick={this.onButtonClick}>
+        <button
+          className="btn btn-outline-secondary"
+          data-filter="all"
+          data-active="false"
+          onClick={this.onButtonClick}
+        >
           All
         </button>
 
-        <button className="btn btn-outline-secondary" data-filter="active" onClick={this.onButtonClick}>
+        <button
+          className="btn btn-outline-secondary"
+          data-filter="active"
+          data-active="false"
+          onClick={this.onButtonClick}
+        >
           Active
         </button>
 
-        <button className="btn btn-outline-secondary" data-filter="done" onClick={this.onButtonClick}>
+        <button
+          className="btn
+          btn-outline-secondary"
+          data-filter="done"
+          data-active="false"
+          onClick={this.onButtonClick}
+        >
           Done
         </button>
       </div>
